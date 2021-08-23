05020193e5726b6fa635889629f03970ec23560c
felipeam10 <felipeam10@hotmail.com>
2021-08-23 01:20:42 -0300

    continuacao do projeto springbootv1

---
+++ src/main/java/br/com/forum/springbootv2/SpringbootV1Application.java
@@ -0,0 +1,14 @@
+package br.com.forum.springbootv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootV1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootV1Application.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/forum/springbootv2/modelo/StatusTopico.java
@@ -0,0 +1,9 @@
+package br.com.forum.springbootv2.modelo;
+
+public enum StatusTopico {
+
+	NAO_RESPONDIDO,
+	NAO_SOLUCIONADO,
+	SOLUCIONADO,
+	FECHADO;
+}
