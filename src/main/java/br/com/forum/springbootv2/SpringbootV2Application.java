3076c4ffd1c31adbb55d672efd068e1d72d8a774
felipeam10 <felipeam10@hotmail.com>
2021-09-01 00:50:29 -0300

    v1.4 simplificando a paginacao e ordenacao

--- src/main/java/br/com/forum/springbootv2/SpringbootV2Application.java
+++ src/main/java/br/com/forum/springbootv2/SpringbootV2Application.java
@@ -2,8 +2,10 @@ package br.com.forum.springbootv2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class SpringbootV2Application {
 
 	public static void main(String[] args) {
