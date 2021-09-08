8f9c01b1dc0ed4ecb4d4ed3f3b65b604e99fc97c
felipeam10 <felipeam10@hotmail.com>
2021-09-08 00:01:54 -0300

    v3.1 Halibitando o spring security

---
+++ src/main/java/br/com/forum/springbootv2/config/security/SecurityConfigurations.java
@@ -0,0 +1,12 @@
+package br.com.forum.springbootv2.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    
+}
